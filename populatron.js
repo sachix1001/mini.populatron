e3164d79cf82249a15776fe2468d477a7f5f56d0
Sachi Grannan <sachix1001@gmail.com>
2020-01-15 18:57:06 +0900

    complete populatron

--- populatron.js
+++ populatron.js
@@ -1,5 +1,21 @@
 const fs = require("fs");
 
+// function onFinished() {
+//   return fs.readFile("cities.csv", "utf8", (error, result) =>
+//     console.log(error, result)
+//   );
+// }
 module.exports = {
-  totalPopulation(onFinished) {},
+  totalPopulation(onFinished) {
+    fs.readFile("cities.csv", "utf8", (error, result) => {
+      if (error) throw error;
+      let total = result
+        .split("\n")
+        .slice(1)
+        .reduce((add, line) => {
+          return (add += Number(line.split(",")[2]));
+        }, 0);
+      onFinished(total);
+    });
+  },
 };
